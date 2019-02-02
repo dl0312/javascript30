fea3ab8eed47b78955cf7eb04f462cbea73a64df
GeonLee <leegun2003@gmail.com>
2019-02-03 02:25:58 +0900

    day2

---
+++ day2-clock/main.js
@@ -0,0 +1,20 @@
+const secondHand = document.querySelector(".sec-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+setDate = () => {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + 90;
+  minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+};
+
+setInterval(setDate, 1000);
